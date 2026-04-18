5795e371b53d2b72fbcd11ba8e442f1d1ec4bb43
Turnip2 <douglassatreyu992@gmail.com>
2026-04-17 21:39:29 -0600

    Add files via upload

---
+++ caine-back-and-forth.py
@@ -0,0 +1,33 @@
+# caine
+
+import time
+
+ 
+print("Hi! My name is Caine! Are YOU ready for some JAW DROPPING, HEART STOPPING, GREATEST MIND-BLOWING PARAPHENELIA YOU HAVE EVER LAID YOUR EYES UPON! (Type help for commands)") 
+
+
+
+user_choice = input("You say: ")
+    
+if user_choice == "I want Angel Food Cake!":
+    print("He says: YOU PARASITE!")
+    
+if user_choice == "We love your adventures!":
+    print("He says: It seems like you're trying to.. WHERE'S KINGER.")
+
+if user_choice == "Let's throw a f-reaking beach party!":
+    print("He says: Good boy, Bubble.")
+
+if user_choice == "Let's throw a f***ing beach party!":
+    print("He says: WHY DO YOU SWEAR NOW? Ugh- Let's just go to intermission.")
+
+if user_choice == "Made with all the love I'm legally allowed to give!":
+    print("He says: A delicious feast made by bubble!")
+
+if user_choice == "help":
+    print("HERE ARE THE COMMANDS LIST! AHAHAHAHA!-   I want Angel Food Cake!  We love your adventures! Let's throw a f-reaking beach party!  Let's throw a f***ing beach party!  Made with all the love I'm legally allowed to give!")
+
+        
+    
+
+
